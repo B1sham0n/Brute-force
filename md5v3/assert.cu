ce1bc79271dde501eb99681ecf7c4ee4f7c79f5f
Yahub <seitasanov.yahub@mail.ru>
2020-02-21 00:08:58 +0300

    last

--- md5v3/assert.cu
+++ md5v3/assert.cu
@@ -19,7 +19,7 @@
 
 #define ERROR_CHECK(X) { gpuAssert((X), __FILE__, __LINE__); }
 #include <iostream>
-inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true){
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort = false){
   if(code != cudaSuccess){
     std::cout << "Error: " << cudaGetErrorString(code) << " " << file << " " << line << std::endl;
     if(abort){
