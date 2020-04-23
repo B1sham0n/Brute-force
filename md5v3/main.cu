e5f4bc5280683825a414bb17c86cd68acdf57590
Valeriya <valeriya.ridchenko@mail.ru>
2020-04-23 19:31:30 +0300

    Новый мд5 и ша1, добавлен hex_to_string()

--- md5v3/main.cu
+++ md5v3/main.cu
@@ -1,4 +1,4 @@
-
+#include <string.h>
 #include <stdio.h>
 #include <iostream>
 //#include <time.h>
@@ -10,6 +10,7 @@
 //#include <curand_kernel.h>
 #include <device_functions.h>
 #include "device_launch_parameters.h"
+
 #define uint8  unsigned char
 
 #define CONST_WORD_LIMIT 10
@@ -30,7 +31,8 @@
 #include "sha256.cu"
 #include "keccak.cu"
 #include "sha1new.cu"
-
+#include "md5new.cu"
+#include "keccakv2.cu"
  /* Global variables */
 uint8_t g_wordLength;
 
@@ -103,7 +105,30 @@ __global__ void md5Crack(uint8_t wordLength, char* charsetWord, uint32_t hash01,
         return;
     }
 }
+__global__ void md5Crack2(uint8_t wordLength, char* charsetWord, uint8* origin) {
+    uint32_t idx = (blockIdx.x * blockDim.x + threadIdx.x);
+    __shared__ char sharedCharset[CONST_CHARSET_LIMIT];
+    char threadCharsetWord[CONST_WORD_LIMIT];
+    uint8 threadTextWord[CONST_WORD_LIMIT], md5sum[16];
+    uint8_t threadWordLength;
+    
+    memcpy(threadCharsetWord, charsetWord, CONST_WORD_LIMIT);
+    memcpy(&threadWordLength, &wordLength, sizeof(uint8_t));
+    memcpy(sharedCharset, g_deviceCharset, sizeof(uint8_t) * CONST_CHARSET_LIMIT);
+    next(&threadWordLength, threadCharsetWord, idx);
 
+    for (uint32_t i = 0; i < threadWordLength; i++) {
+        threadTextWord[i] = sharedCharset[threadCharsetWord[i]];
+    }
+
+    md5new(threadTextWord, +wordLength, md5sum);
+    if (compare(md5sum, origin, 16)) {
+        memcpy(g_deviceCracked, threadTextWord, wordLength);
+    }
+    if (!next(&threadWordLength, threadCharsetWord, 1)) {
+        return;
+    }
+}
 __global__ void sha1Crack(uint8_t wordLength, char* charsetWord, uint32_t hash01, uint32_t hash02, uint32_t hash03, uint32_t hash04, uint32_t hash05) {
     uint32_t idx = (blockIdx.x * blockDim.x + threadIdx.x);
     __shared__ char sharedCharset[CONST_CHARSET_LIMIT];
@@ -181,7 +206,7 @@ __global__ void sha256Crack(uint8_t wordLength, char* charsetWord, uint8* unhexe
     }
 }
 
-void hex_to_string(uint8* msg, size_t msg_sz, char* hex, size_t hex_sz)
+void string_to_hex(uint8* msg, size_t msg_sz, char* hex, size_t hex_sz)
 {
     memset(msg, '\0', msg_sz);
     for (int i = 0; i < hex_sz; i += 2)
@@ -271,9 +296,114 @@ void gpu_init() {
     BLOCKS = block_size * device_prop.multiProcessorCount;
     //int clock_speed = (int)(device_prop.memoryClockRate * 1000 * 1000);
 }
+static void test_blake2b_wo_key(char word[4])
+{
+    uint32 test_block_size[] = { 16,32,64 };
+    uint32 INPUT_SIZE = 4;
+    uint8* KEY = NULL;
+    uint32 KEYLEN = 0;
 
+    uint32 BLAKE2B_BLOCK_SIZE = test_block_size[1];
+    // TEST BLAKE2B
+    /*BYTE blake2b_inp[INPUT_SIZE*1024];
+    BYTE blake2b_oup[1024*BLAKE2B_BLOCK_SIZE];*/
+    uint8* blake2b_inp = (uint8*)malloc(INPUT_SIZE * 1024 * sizeof(BYTE));
+    uint8* blake2b_oup = (uint8*)malloc(1024 * BLAKE2B_BLOCK_SIZE * sizeof(BYTE));
+    srand(0);
+    for (unsigned int i = 0; i < 1024 * INPUT_SIZE; i++)
+    {
+        blake2b_inp[i] = rand() % 256;
+    }
+    // CPU hash
+    for (int i = 0; i < 1024; i++)
+    {
+        keccaknew((unsigned char*)word, INPUT_SIZE, blake2b_oup + BLAKE2B_BLOCK_SIZE * i, BLAKE2B_BLOCK_SIZE << 3);
+    }
+
+    //BYTE blake2b_cu_oup[1024*BLAKE2B_BLOCK_SIZE];
+    BYTE* blake2b_cu_oup = (BYTE*)malloc(1024 * BLAKE2B_BLOCK_SIZE * sizeof(BYTE));
+    keccaknew((unsigned char*)word, INPUT_SIZE, blake2b_cu_oup, BLAKE2B_BLOCK_SIZE << 3);
+
+    if (memcmp(blake2b_oup, blake2b_cu_oup, BLAKE2B_BLOCK_SIZE * 1024) != 0)
+    {
+        printf("Failed test BLAKE2B no key, len %u \n", BLAKE2B_BLOCK_SIZE);
+    }
+    else
+    {
+        printf("Passed test BLAKE2B no key, len %u \n", BLAKE2B_BLOCK_SIZE);
+    }
+    free(blake2b_inp);
+    free(blake2b_oup);
+    free(blake2b_cu_oup);
+}
+char* phex(const void* p, size_t n)
+{
+    const unsigned char* cp = (unsigned char*) p;              /* Access as bytes. */
+    char* s = (char*) malloc(2 * n + 1);       /* 2*n hex digits, plus NUL. */
+    size_t k;
+
+    /*
+     * Just in case - if allocation failed.
+     */
+    if (s == NULL)
+        return s;
+
+    for (k = 0; k < n; ++k) {
+        /*
+         * Convert one byte of data into two hex-digit characters.
+         */
+        sprintf(s + 2 * k, "%02X", cp[k]);
+    }
+
+    /*
+     * Terminate the string with a NUL character.
+     */
+    s[2 * n] = '\0';
+
+    return s;
+}
+
+__device__ __host__ void makedigits(unsigned char x, unsigned char(&digits)[2])
+{
+    unsigned char d0 = x / 16;
+    digits[1] = x - d0 * 16;
+    unsigned char d1 = d0 / 16;
+    digits[0] = d0 - d1 * 16;
+}
+
+__device__ __host__ void makehex(unsigned char(&digits)[2], char(&hex)[2])
+{
+    for (int i = 0; i < 2; ++i) {
+        if (digits[i] < 10) {
+            hex[i] = '0' + digits[i];
+        }
+        else {
+            hex[i] = 'a' + (digits[i] - 10);
+        }
+    }
+}
+
+__device__ __host__ void hex_to_string(unsigned char* input, char* output, int size)
+{
+
+    for (int i = 0; i < 16; ++i) {
+        unsigned char val = input[i];
+        unsigned char d[2];
+        char h[2];
+        makedigits(val, d);
+        makehex(d, h);
+        output[2 * i] = h[0];
+        output[2 * i + 1] = h[1];
+    }
+}
 
 int main(int argc, char* argv[]) {
+    /*char* w = new char[4];
+    w[0] = 'k';
+    w[1] = 'i';
+    w[2] = 's';
+    w[3] = 'a';
+    test_blake2b_wo_key(w);*/
     char* hash;// = "e9d71f5ee7c92d6dc9e92ffdad17b8bd49418f98";
   /*  unsigned char* sha3hash = new unsigned char[64];
     char* word = "kisa";
@@ -285,18 +415,87 @@ int main(int argc, char* argv[]) {
     std::cout << "15: " << std::hex << 15;*/
 
 
-   /*char* testhash = "cb990257247b592eaaed54b84b32d96b7904fd95";
+   /*char* testhash = "775a2dae52c3ef080726dd427f81bd482fdf96b5";
    char* word = "zzzz";
-   unsigned char* sha1hash = new unsigned char[40];
+   unsigned char* sha1hash = new unsigned char[41];
    sha1new((unsigned char*)word, 4, sha1hash);
    uint8 sha1Unhexed[21];
    hex_to_string(sha1Unhexed, 20, testhash, 40);
    for (int i = 0; i < 20; i++)
    {
        if (sha1Unhexed[i] != sha1hash[i])
-           std::cout << "noooo" << std::endl;
-   }
-   */   
+           std::cout << "not equal" << std::endl;
+   }*/
+   
+
+    char* testhash = "1c0d894f6f6ab511099a568f6e876c2f";
+    char* word = "kisa";
+    unsigned char* md5Hash = new unsigned char[32];
+    md5new((unsigned char*)word, 4, md5Hash);
+    uint8 md5unh[16];
+    string_to_hex(md5unh, 16, testhash, 32);
+    for (int i = 0; i < 16; i++)
+    {
+        if (md5unh[i] != md5Hash[i]);
+            //std::cout << "not equal" << std::endl;
+    }
+    char str[16][2];
+    char* new_word = (char*) malloc(sizeof(char)*32);
+    for (int j = 0; j < 16; ++j) {
+        sprintf(str[j], "%02x", md5Hash[j]);
+    }
+    
+    for(int i = 0; i < 16; i++)
+        std::cout << str[i][0] << str[i][1];
+
+    std::cout << std::endl;
+
+    unsigned char* d_md5Hash;
+    cudaMalloc(&d_md5Hash, 32 * sizeof(unsigned char));
+    cudaMemcpy(d_md5Hash, &md5Hash[0], 32 * sizeof(unsigned char),
+        cudaMemcpyHostToDevice);
+
+    char* d_str1;
+    cudaMalloc(&d_str1, 32 * sizeof(char));
+    char str1[32];
+    //kernel<<<1,1>>>(d_md5Hash, d_str1);
+    cudaMemcpy(&str1[0], d_str1, 32 * sizeof(char), cudaMemcpyDeviceToHost);
+    for (int i = 0; i < 32; i += 2) std::cout << str1[i] << str1[i + 1];
+    std::cout << std::endl;
+
+    /*bool next_ = true;
+    int k = 0;
+    int m = 0;
+    while (next_) {
+        new_word[k] = str[m][0];
+        new_word[k + 1] = str[m][1];
+        k += 2;
+        m++;
+        if (k >= 32)
+            next_ = false;
+    }
+    */
+    for (int i = 0; i < 16; i++) {
+        new_word[2 * i] = str[i][0];
+        new_word[2 * i + 1] = str[i][1];
+    }
+
+    for (int i = 0; i < 32; i++)
+        std::cout << new_word[i];
+    std::cout << std::endl;
+    unsigned char* md5Hash2 = new unsigned char[32];
+    md5new((unsigned char*)new_word, 32, md5Hash2);
+    for (int i = 0; i < 16; i++)
+    {
+        if (md5unh[i] != md5Hash2[i]);
+        //std::cout << "not equal" << std::endl;
+    }
+    char str2[16][1];
+    for (int j = 0; j < 16; ++j) {
+        //printf("%02x", md5Hashh[j]);
+        sprintf(str2[j], "%02x", md5Hash2[j]);
+        std::cout << str2[j];
+    }
    /* Check arguments */
    if (argc != 2) {
         std::cout << "Need hash password. Now arguments count: " << argc << std::endl;
@@ -320,13 +519,16 @@ int main(int argc, char* argv[]) {
     std::cout << "|    " << THREADS << " threads found" << std::endl;
     std::cout << "|**********************/" << std::endl;
 
-    uint32_t md5Hash[4];
+    //uint32_t md5Hash[4];
     uint32_t sha1Hash[5];
     uint8 sha256Unhexed[33];
     uint8* unh;
 
     uint8 sha1Unh[21];
     uint8* sha1_;
+
+    uint8 md5Unh[16];
+    uint8* md5_;
 
     switch (hash_size) {
     case 32: 
@@ -338,6 +540,10 @@ int main(int argc, char* argv[]) {
             sscanf(tmp, "%x", &md5Hash[i]);
             md5Hash[i] = (md5Hash[i] & 0xFF000000) >> 24 | (md5Hash[i] & 0x00FF0000) >> 8 | (md5Hash[i] & 0x0000FF00) << 8 | (md5Hash[i] & 0x000000FF) << 24;
         }
+        memset(md5Unh, 0, 16);
+        hex_to_string(md5Unh, 16, hash, 32);
+        cudaMalloc((char**)&md5_, sizeof(char) * 16);
+        cudaMemcpy(md5_, md5Unh, sizeof(char) * 16, cudaMemcpyHostToDevice);
         break;
     case 40: 
         /* Parse argument (sha1) */ 
@@ -417,8 +623,9 @@ int main(int argc, char* argv[]) {
             /* Start kernel */
             switch (hash_size) {
             case 32: 
-                md5Crack <<< BLOCKS, THREADS >>> (g_wordLength, words[device], md5Hash[0], md5Hash[1], 
-                    md5Hash[2], md5Hash[3]);
+                //md5Crack <<< BLOCKS, THREADS >>> (g_wordLength, words[device], md5Hash[0], md5Hash[1], 
+                //    md5Hash[2], md5Hash[3]);
+                md5Crack2 << < BLOCKS, THREADS >> > (g_wordLength, words[device], md5_);
                 break;
             case 40: 
                 //sha1Crack <<< BLOCKS, THREADS >>> (g_wordLength, words[device], sha1Hash[0], sha1Hash[1], 
